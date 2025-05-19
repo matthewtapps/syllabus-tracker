ccc79e72907266ac2ecabbf5981b55d11033e66f
Matthew Tapps <mail@matthewtapps.com>
2025-05-19 11:25:55 +1000

    Remove host header for frontend traffic redirection

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -176,7 +176,6 @@ in
       locations."/" = {
         proxyPass = "http://127.0.0.1:3001/";
         extraConfig = ''
-          proxy_set_header Host $host;
           proxy_set_header X-Real-IP $remote_addr;
           proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
           proxy_set_header X-Forwarded-Proto $scheme;
