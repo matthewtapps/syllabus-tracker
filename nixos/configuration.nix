65f5f15e379bf82044c94fbf3d94d6fe907c4b04
Matthew Tapps <mail@matthewtapps.com>
2025-05-20 11:01:23 +1000

    Cluster-level nginx, while keeping host-level SSL certificate management

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -172,23 +172,16 @@ in
     virtualHosts.${domain} = {
       forceSSL = true;
       enableACME = true;
-      # Proxy requests to application containers
       locations."/" = {
-        proxyPass = "http://127.0.0.1:3001/";
-        extraConfig = ''
-          proxy_set_header X-Real-IP $remote_addr;
-          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-          proxy_set_header X-Forwarded-Proto $scheme;
-        '';
-      };
-      locations."/api/" = {
-        proxyPass = "http://127.0.0.1:8001/api/";
-        proxyWebsockets = true;
+        proxyPass = "http://127.0.0.1:8080/";
         extraConfig = ''
           proxy_set_header Host $host;
           proxy_set_header X-Real-IP $remote_addr;
           proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
           proxy_set_header X-Forwarded-Proto $scheme;
+          proxy_http_version 1.1;
+          proxy_set_header Upgrade $http_upgrade;
+          proxy_set_header Connection "upgrade";
         '';
       };
     };
