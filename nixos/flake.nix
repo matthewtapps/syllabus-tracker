2c8869eb68bfe5a4bef3e3599a0b873e97618565
Matt <mail@matthewtapps.com>
2026-06-02 07:46:38 +1000

    Bump nix flake version to fix nginx otel

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,14 +2,11 @@
   description = "Syllabus Tracker NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
     # Community flake that packages ngx_otel_module as a NixOS module.
-    # We only consume its package output, not its NixOS module: that
-    # module relies on `services.nginx.prependConfig`, which doesn't
-    # exist until nixos-unstable. We work around it by injecting
-    # `load_module` via the systemd ExecStart `-g` flag instead (see
-    # configuration.nix).
+    # Provides services.nginx.otel.*; relies on services.nginx.prependConfig
+    # (available from nixos-25.11 onward).
     nginx-otel = {
       url = "github:djvcom/nix-nginx-otel";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -19,10 +16,10 @@
   outputs = { self, nixpkgs, nginx-otel }: {
     nixosConfigurations.syllabustracker-nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      specialArgs = { inherit nginx-otel; };
       modules = [
         ./configuration.nix
         ./hardware-configuration.nix
+        nginx-otel.nixosModules.default
       ];
     };
   };
